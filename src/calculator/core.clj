255c9a22375a36156e06115a8358bbe108de858b
jurko <jurko.bossetroll@gmail.com>
2023-02-24 12:14:32 +0100

    Refactoring

--- src/calculator/core.clj
+++ src/calculator/core.clj
@@ -12,13 +12,13 @@
     :DIV /))
 
 
-(defn calculate
+(defn calculate-expression
   [[action operand-1 operand-2]]
   (case action
     :NUMBER (parser/parse-number operand-1)
-    :PARENTH (calculate operand-1)
+    :PARENTH (calculate-expression operand-1)
     (apply (operation action)
-           (map calculate [operand-1 operand-2]))))
+           (map calculate-expression [operand-1 operand-2]))))
 
 
 ;(defn calculate
@@ -34,10 +34,16 @@
   "Gets graph vector from parser/parse-expressionq"
   [[ident data]]
   (case ident
-    :EXPRESSION {:result (calculate data)}
+    :EXPRESSION {:result (calculate-expression data)}
     :EMPTY      {:error "expression is empty"}
     :ERROR      {:error "expression is incorrect"}
     {:fail "instaparser haven't return any graph, nether error"}))
+
+
+(defn calculate
+  [expression-string]
+  (calculator
+    (parser/parse-expression expression-string)))
 
 
 (comment
