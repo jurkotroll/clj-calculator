3050e59edd59d12da63a0dd5903cf4978f30c735
jurko <jurko.bossetroll@gmail.com>
2023-09-24 10:19:06 +0200

    Refactoring

--- src/user.clj
+++ src/user.clj
@@ -1,8 +1,12 @@
 (ns user
-  (:require [portal.api :as p]))
+  (:require [portal.api :as p]
+            [server.core :as server]))
+
+(def p (p/open {:launcher :intellij}))
 
 
 (add-tap #'p/submit)
 
+(tap> "Hello Portal")
 
 (def p (p/open {:launcher :intellij}))
